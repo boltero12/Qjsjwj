7e87219e8e7ef5064eb9578116c494467eb9eb40
boltero12 <boltero12@gmail.com>
2025-05-22 21:27:32 -0300

    Update design to be more minimal

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'xs': ['0.75rem', { lineHeight: '1rem' }],
+        'sm': ['0.8125rem', { lineHeight: '1.25rem' }],
+        'base': ['0.875rem', { lineHeight: '1.5rem' }],
+        'lg': ['1rem', { lineHeight: '1.75rem' }],
+        'xl': ['1.125rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.25rem', { lineHeight: '2rem' }],
+        '3xl': ['1.5rem', { lineHeight: '2rem' }],
+        '4xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      },
       colors: {
         'soft-blue': {
           50: '#f0f7ff',
@@ -41,6 +51,10 @@ export default {
           'Roboto',
           'sans-serif',
         ],
+      },
+      spacing: {
+        '4.5': '1.125rem',
+        '5.5': '1.375rem',
       },
     },
   },
